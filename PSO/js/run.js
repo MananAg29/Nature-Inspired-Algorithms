c067b9c92f8063f5841f5eec68bfe1e30035cd66
Manan Agarwal <55892621+MananAg29@users.noreply.github.com>
2021-12-11 17:38:51 +0530

    Add files via upload

---
+++ PSO/js/run.js
@@ -0,0 +1,53 @@
+require.config({
+    baseUrl: 'js',
+    paths: {
+        // Libs
+        'underscore': 'lib/underscore',
+        'jquery': 'lib/jquery-2.0.3.min',
+        'requestAnimationFrame': 'lib/requestAnimationFrame',
+        'fastclick': 'lib/fastclick',
+        
+        // My modules
+        'app': 'app/app',
+        'viewport': 'app/viewport',
+        'pso': 'app/pso',
+        'tween': 'app/tween',
+        'util': 'app/util',
+        'vect': 'app/vect'
+    },
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+    }
+});
+
+require([
+    'requestAnimationFrame',
+    'fastclick',
+    'app'
+    ], function(RAF, FastClick, App) {
+    RAF.initialize();
+
+    FastClick.attach(document.body);
+
+    // Define behaviors
+    var buttons = $('#buttons');
+    buttons.find('button[name="start"]').on('click', function() {
+        App.start();
+        $('#clickStart').hide();
+    });
+    buttons.find('button[name="reset"]').on('click', function() {
+        App.resetForm();
+    });
+    buttons.find('button[name="stop"]').on('click', function() {
+        console.log("stop!");
+        App.stop();
+    });
+    $('#liveOptionsInput input:checkbox').on('change', function() {
+        App.readLiveOptions();
+    });
+
+    App.resetForm(true);
+});
+
